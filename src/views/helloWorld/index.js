e3e107c7c75d165ff3dffa590314a03447afe934
Kevin D'Ornellas <kdornellas@aviationresearch.com>
2019-04-24 11:26:07 -0600

    added forgotten components

---
+++ src/views/helloWorld/index.js
@@ -0,0 +1,10 @@
+import './css/style.css'
+import './JS/index.js'
+import './index.html'
+
+if(typeof(module.hot) !== 'undefined') {
+  module.hot.accept() // eslint-disable-line no-undef  
+}
+
+
+
